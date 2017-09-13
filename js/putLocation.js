7c004bf15fbd2a290df2b4ac3cdbc6cbcb8c480e
youn410 <youn410@gmail.com>
2017-09-13 01:23:58 +0000

    get location from mysql

---
+++ js/putLocation.js
@@ -0,0 +1,33 @@
+$(function(){
+
+	var map = new GMaps(
+		{
+			div: '#map',
+			lat: 35.714926,
+			lng: 139.761919,
+			zoom: 14
+		}
+	);
+
+
+	setInterval(function(){
+		$.getJSON("/cgi-bin/getLocation.cgi", function(data){
+			console.log(data);
+
+			for(rider of data){
+				console.log(rider)
+		
+				map.addMarker(
+					{
+						lat: parseFloat(rider.lat),
+						lng: parseFloat(rider.lng),
+						title: rider.name,
+						icon: "/icons/tktbtk.jpg"
+					}
+				);
+			}
+
+		});
+	},5000);
+
+});
---
+++ modules/sample.py
@@ -0,0 +1,13 @@
+
+import json
+
+print "test"
+
+"""
+print json.dumps(
+		[
+			{'id': 1, 'name': 'tktbtk'},
+			{'id': 2, 'name': 'nomu'},
+		]
+	)
+"""
